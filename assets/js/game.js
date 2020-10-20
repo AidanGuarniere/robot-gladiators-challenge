2b95f1a5a011fd7eb5d80894958328aa0dc08bf5
aidan <aidanguarniere@gmail.com>
2020-10-20 10:56:33 -0700

    added console logs for var playerName

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight" //
+function fight() {
+    // creates starting window propt //
+    window.alert("The fight has begun!");
+    // creates playerName prompt and logs it //
+    var playerName = window.prompt("What is your robot's name?");
+    console.log(playerName) ;
+    // this creates a string //
+    console.log("This logs a string, good for leaving yourself a message");
+    // this will do math and log 20
+    console.log(10 + 10);
+    // creates a string concatenation that announces playerName //
+    console.log("Our robot's name is " + playerName);
+}
+fight();
